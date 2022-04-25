9077d4a158deda563455f0afda172625a6af8111
rawinolivera <oli18001@byui.edu>
2022-04-25 12:47:11 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const links = [
     {
         label: "Week1 notes",
-        url: "week1/indexedDB.html"
+        url: "rawinolivera.github.io/wdd331r/week1/indexedDB.html"
     }
 ]
+
+let olist = document.querySelector("ol");
+links.forEach(displayLinks);
+
+function displayLinks (link) {
+    let list = document.createElement('li');
+    let a = document.createElement('a');
+
+    list.textContent = `${link.label}`;
+    a.textContent = `"${link.url}"`;
+    
+    list.appendChild(a); 
+    olist.appendChild(list);
+    
+}
